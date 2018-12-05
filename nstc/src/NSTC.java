69c6de4c32fcd00e31568ddfb9ecf8028fd710c1
mfernandriu <munozfernandezandres@gmail.com>
2018-12-05 01:10:19 +0100

    Servlet modificado y listo para que se le añadan las invocaciones tipo cliente rest

--- nstc/src/NSTC.java
+++ nstc/src/NSTC.java
@@ -1,15 +1,21 @@
+
+
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Enumeration;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.swing.text.html.HTML;
 
-public class Servlet extends HttpServlet {
+/**
+ * Servlet implementation class NSTC
+ */
+@WebServlet("/NSTC")
+public class NSTC extends HttpServlet {
 
 	public void init() throws ServletException {
 
@@ -23,7 +29,7 @@ public class Servlet extends HttpServlet {
 		//output
 		PrintWriter out = res.getWriter();
 
-		//nÃºmero de parametros
+		//número de parametros
 		Enumeration<String> paramE = req.getParameterNames();
 		ArrayList<String> paramL = new ArrayList<String>();
 
@@ -34,8 +40,13 @@ public class Servlet extends HttpServlet {
 
 		int numParam = paramL.size();
 
-		//parÃ¡metros
+		//parámetros
 		String pantalla = req.getParameter("pantalla");
+		String url = req.getParameter("url");
+		String field = req.getParameter("field");
+		String level = req.getParameter("level");
+		String name1 = req.getParameter("name1");
+		String name2 = req.getParameter("name2");
 
 		if(numParam == 0) {
 
@@ -52,17 +63,26 @@ public class Servlet extends HttpServlet {
 
 			case "response1":
 
-				printResponse1(out, res);
+				if(url == null)
+					printError(out, res, "Parámetros incorrectos para la pantalla response1");
+				String response1 = request1(url);
+				printResponse1(out, res, response1, url);
 				break;
 
 			case "response2":
 
-				printResponse2(out, res);
+				if(url == null || field == null || level == null)
+					printError(out, res, "Parámetros incorrectos para la pantalla response2");
+				String[] response2 = request2(url, field, level);
+				printResponse2(out, res, response2, url, field, level);
 				break;
 
 			case "response3":
 
-				printResponse3(out, res);
+				if(name1 == null || name2 == null)
+					printError(out, res, "Parámetros incorrectos para la pantalla response3");
+				int response3 = request3(name1, name2);
+				printResponse3(out, res, response3, name1, name2);
 				break;
 
 			default:
@@ -75,8 +95,56 @@ public class Servlet extends HttpServlet {
 
 	}
 
+	private int request3(String name1, String name2) {
+		
+		int grade;
+		
+		//aqui hay que poner la invo TODO
+		grade = 1;
+		
+		return grade;
+	}
+
+	private String[] request2(String url, String field, String level) {
+		
+		String response2String;
+		
+		if(field.equals("")) {
+			field = "None";
+		}
+		
+		//aqui hay que poner la invo TODO
+		response2String = "esto#es#un#ejemplo#";
+		
+		String[] response2Array = parseRequest2(response2String);
+		
+		return response2Array;
+	}
+
+	private String[] parseRequest2(String response2String) {
+
+		String[] parts = response2String.split("#");
+		
+		for(String part : parts) {
+			System.out.println("part " + part);
+		}
+		
+		return parts;
+	}
+
+	private String request1(String url) {
+		
+		//aquí hay que poner la invocación
+		
+		String xmlCode;
+		
+		//aqui hay que poner la invo TODO
+		xmlCode = "De momento este es el código xml jajajaj";
+		
+		return xmlCode;
+	}
+
 	private void printRequest(PrintWriter out, HttpServletResponse res) {
-		// TODO Auto-generated method stub
 		
 		printHeader(out, res);
 		
@@ -139,50 +207,121 @@ public class Servlet extends HttpServlet {
 		printFooter(out, res);
 	}
 
-	private void printResponse1(PrintWriter out, HttpServletResponse res) {
-		// TODO Auto-generated method stub
+	private void printResponse1(PrintWriter out, HttpServletResponse res, String response1, String url) {
 		
-		//construye la tabla
-		String[] cabeceras;
-		String[][] valores;
-		
-		cabeceras = new String[]{"Campo", "Valores"};
-		
-		String[] array1 = new String[] {"Nombre", "Beatriz"};
-		String[] array2 = new String[] {"Amigos", "Manuel, AntÃ³n"};
-		
-		valores = new String[][]{array1, array2};
-		
-		//print
 		printHeader(out, res);
 		
 		out.println("<h1> Response 1 </h1>");
 		
-		printTable(out, res, cabeceras, valores);
+		//form req1
+		out.println("<form action=\"" + this.getServletName() + "\">");
+
+
+		out.println("<h3> request 1 </h3>");
+		out.println("<input type=\"text\" name=\"pantalla\" value=\"response1\"  hidden>");
+
+		out.println("url");
+		out.println("<input type=\"text\" name=\"url\"><br>");
 		
-		printToRequestButton(out, res);
+		out.println("<input type=\"submit\" value=\"Send\">");
+
+		out.println("</form>");
+		//form req1 end
+		
+		//returned data from request1
+		out.println("<textarea>");
+		
+		out.println(response1);
+		
+		out.println("</textarea>");
+		
+		
+		//form req2
+		out.println("<form action=\"" + this.getServletName() + "\">");
+
+		out.println("<h3> request 2 </h3>");
+		out.println("<input type=\"text\" name=\"pantalla\" value=\"response2\"  hidden>");
+		
+		out.println("url");
+		out.println("<input type=\"text\" name=\"url\"><br>");
+		
+		out.println("field");
+		out.println("<input type=\"text\" name=\"field\"><br>");
+		
+		out.println("level");
+		out.println("<input type=\"text\" name=\"level\"><br>");
+
+		out.println("<input type=\"submit\" value=\"Send\">");
+
+		out.println("</form>");
+		//form req2 end
+		
+		
+		//form req3
+		out.println("<form action=\"" + this.getServletName() + "\">");
+
+		out.println("<h3> request 3 </h3>");
+		out.println("<input type=\"text\" name=\"pantalla\" value=\"response3\"  hidden>");
+		
+		out.println("name1");
+		out.println("<input type=\"text\" name=\"name1\"><br>");
+		
+		out.println("name2");
+		out.println("<input type=\"text\" name=\"name2\"><br>");
+		
+		out.println("<input type=\"submit\" value=\"Send\">");
+
+		out.println("</form>");
+		//form req3 end
 		
 		printFooter(out, res);
 	}
+	
+//	private void printResponse1(PrintWriter out, HttpServletResponse res) {
+//		// TODO Auto-generated method stub
+//		
+//		//construye la tabla
+//		String[] cabeceras;
+//		String[][] valores;
+//		
+//		cabeceras = new String[]{"Campo", "Valores"};
+//		
+//		String[] array1 = new String[] {"Nombre", "Beatriz"};
+//		String[] array2 = new String[] {"Amigos", "Manuel, Antón"};
+//		
+//		valores = new String[][]{array1, array2};
+//		
+//		//print
+//		printHeader(out, res);
+//		
+//		out.println("<h1> Response 1 </h1>");
+//		
+//		printTable(out, res, cabeceras, valores);
+//		
+//		printToRequestButton(out, res);
+//		
+//		printFooter(out, res);
+//	}
 
-	private void printResponse2(PrintWriter out, HttpServletResponse res) {
-		// TODO Auto-generated method stub
+	private void printResponse2(PrintWriter out, HttpServletResponse res, String[] response2, String url, String field, String level) {
 
 		//construye la tabla
 		String[] cabeceras;
 		String[][] valores;
 		
 		cabeceras = new String[]{"Nombre"};
+		valores = new String[response2.length][1];
 		
-		String[] array1 = new String[] {"Jose"};
-		String[] array2 = new String[] {"Luis"};
-		
-		valores = new String[][]{array1, array2};
+		for (int i = 0; i < valores.length; i++) {
+			valores[i][0] = response2[i];
+		}
 		
 		//print
 		printHeader(out, res);
 		
 		out.println("<h1> Response 2 </h1>");
+
+		out.println("<h3> For: " + url + " " + field + " " + level + " </h3>");
 		
 		printTable(out, res, cabeceras,valores);
 		
@@ -192,7 +331,6 @@ public class Servlet extends HttpServlet {
 	}
 
 	private void printTable(PrintWriter out, HttpServletResponse res, String[] cabeceras, String[][] valores) {
-		// TODO Auto-generated method stub
 		
 		out.println("<table style=\"width:100%\">");
 		
@@ -219,14 +357,12 @@ public class Servlet extends HttpServlet {
 		
 	}
 
-	private void printResponse3(PrintWriter out, HttpServletResponse res) {
-		// TODO Auto-generated method stub
-		
-		int grado = 0;
+	private void printResponse3(PrintWriter out, HttpServletResponse res, int grado, String name1, String name2) {
 		
 		printHeader(out, res);
 		
 		out.println("<h1> Response 3 </h1>");
+		out.println("<h3> For: " + name1 + " " + name2 + " </h3>");
 		out.println("<h3> Grado: " + grado + " </h3>");
 		
 		printToRequestButton(out, res);
@@ -235,8 +371,7 @@ public class Servlet extends HttpServlet {
 	}
 
 	private void printError(PrintWriter out, HttpServletResponse res, String error) {
-		// TODO Auto-generated method stub
-
+		
 		printHeader(out, res);
 		
 		out.println("<h1> Error </h1>");
@@ -250,7 +385,6 @@ public class Servlet extends HttpServlet {
 	}
 
 	private void printToRequestButton(PrintWriter out, HttpServletResponse res) {
-		// TODO Auto-generated method stub
 		
 		out.println("<form action=\"" + this.getServletName() + "\">");
 
@@ -262,7 +396,6 @@ public class Servlet extends HttpServlet {
 	}
 
 	private void printHeader(PrintWriter out, HttpServletResponse res) {
-		// TODO Auto-generated method stub
 		
 		out.println("<html>");
 
@@ -276,10 +409,8 @@ public class Servlet extends HttpServlet {
 	}
 
 	private void printFooter(PrintWriter out, HttpServletResponse res) {
-		// TODO Auto-generated method stub
-
+		
 		out.println("</body>");
 		out.println("</html>");
 	}
-
 }
